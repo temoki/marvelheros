6565571786752d9558b4263ebe96931c00fe2456
Tomoki Kobayashi <temoki@gmail.com>
2021-08-05 15:28:08 +0900

    SwiftUIプロジェクト作成

---
+++ Marvelheros/Shared/MarvelherosApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelherosApp.swift
+//  Shared
+//
+//  Created by Tomoki Kobayashi on 2021/08/05.
+//
+
+import SwiftUI
+
+@main
+struct MarvelherosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
