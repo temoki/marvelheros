75ef4d39f9a4c008d9223422d5b59a428ca30358
Tomoki Kobayashi <temoki@gmail.com>
2021-08-14 08:34:36 +0900

    レイアウト調整

--- Marvelheros/Shared/Scene/Characters/CharacterView.swift
+++ Marvelheros/Shared/Scene/Characters/CharacterView.swift
@@ -4,13 +4,22 @@ struct CharacterView: View {
     @State var character: CharacterEntity
     
     var body: some View {
-        VStack {
+        VStack(alignment: .leading, spacing: 0) {
             AsyncImage(url: character.thumbnail.url) {
                 $0.resizable().scaledToFit()
             } placeholder: {
-                ProgressView()
+                Color.gray
             }
-            Text(character.name)
+            .aspectRatio(1, contentMode: .fill)
+            
+            Rectangle()
+                .foregroundColor(.red)
+                .frame(height: 6)
+
+            Text(character.name.uppercased())
+                .font(.system(size: 18, weight: .heavy, design: .monospaced))
+                .multilineTextAlignment(.leading)
+                .padding(8)
         }
     }
 }
